dbe153b50ef78851dded525a3b16a932526a4120
ivand <uo282394@uniovi.es>
2023-02-06 12:24:19 +0100

    commit inicial para enlazar github

---
+++ src/test/java/com/uniovi/notaineitor/Sdi2223812SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.notaineitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2223812SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
